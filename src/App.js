21bd5e9efb1a5944c49601502b86e37a8acbe410
John Popoola <johnbimbo12@gmail.com>
2023-06-23 22:07:31 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Header from './Header';
+import SwipeButtons from './SwipeButtons';
+import TinderCards from './TinderCards';
+
+function App() {
+  return (
+    //BEM class naming convention
+    <div className="app">
+      
+      {/* Header */}
+      <Header />
+      {/* TinderCards */}
+      <TinderCards />
+      {/* SwipeButtons */}
+      <SwipeButtons />
+    </div>
+  );
+}
+
+export default App;
